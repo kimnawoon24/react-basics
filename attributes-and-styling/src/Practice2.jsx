99932116c5eb8e5013461de2b4af860203c0c169
nawoon kim <kimnawoon24@gmail.com>
2026-01-25 23:55:17 +0900

    실습 파일 추가

---
+++ attributes-and-styling/src/Practice2.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+ return (
+    <>
+      <span style={
+        {
+          fontWeight: "bold",
+          fontStyle: "italic"
+        }
+      }>
+        Bold & Italic
+      </span>
+    </>
+  )
+}
+
+export default App
