0b5071428e1bbc8e2fecfddc9777eca0869e3f00
nawoon kim <kimnawoon24@gmail.com>
2026-01-25 23:52:53 +0900

    실습 파일 추가

---
+++ attributes-and-styling/src/Practice1.jsx
@@ -0,0 +1,29 @@
+function Practice1() {
+  return (
+    <>
+        <label
+            htmlFor="username"        // for
+            Username:
+        >
+        </label>
+        <input
+            type = "text"
+            id = "username"
+            className="input-field"   // class
+            autoComplete="off"        // autocomplete
+
+            maxLength={20}            // maxlength
+            spellCheck={true}         // spellcheck
+            readOnly={false}          // readonly
+            tabIndex={0}              // tabindex
+
+            disabled = (disableInput)
+            placeholder = (
+              disableInput ? "(DISABLED)" : "Enter your name."
+        )
+        />
+    </>
+  )
+}
+
+export default Practice1; 
